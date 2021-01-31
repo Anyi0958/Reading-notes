2951c265501c5007d971d96c058ba335514e28c1
fwx_php <279357596@qq.com>
2021-02-01 00:47:32 +0800

    add Linux instructions of the linux by uncle.bird

---
+++ NotesmarkdownGeneraor.bat
@@ -0,0 +1,13 @@
+@echo off && setlocal enabledelayedexpansion
+
+set /p "filename=filename: "
+
+mkdir %filename%
+cd %filename%
+
+echo %filename% д©б╪>>%filename%.md
+echo ^[TOC^]>>%filename%.md
+echo ^*^*^*>>%filename%.md
+echo.
+echo ^# г╟ят>>%filename%.md
+echo.
