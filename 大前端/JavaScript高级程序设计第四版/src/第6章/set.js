ae29c5eb58140fe441d3583f28448a6593928de5
fwx_php <279357596@qq.com>
2021-02-09 19:52:34 +0800

    modify notes of Js

---
+++ 大前端/JavaScript高级程序设计第四版/src/第6章/set.js
@@ -0,0 +1,14 @@
+const s1 = new Set(["val1", "val2", "val3"]);
+
+console.log(s1.size);   // 3
+
+// 使用自定义迭代器初始化集合
+const s2 = new Set({
+    [Symbol.iterator]: function* () {
+        yield "val1";
+        yield "val2";
+        yield "val3";        
+    }
+});
+
+console.log(s2.size);   // 3
