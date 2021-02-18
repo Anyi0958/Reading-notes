92a9e4709556143afc1bae6e17d13867fb9cc151
fwx_php <279357596@qq.com>
2021-02-19 01:51:13 +0800

    add auto-push tool

---
+++ 大前端/git自动push.bat
@@ -0,0 +1,16 @@
+@echo off && setlocal enabledelayedexpansion
+
+git status
+set /p "info=commit info:"
+git add .
+git commit -am "%info%"
+pause
+git status
+pause
+git switch master
+git merge dev
+pause
+git push origin master
+pause
+git switch dev
+git status
